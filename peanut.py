450599b186302c125bb204642cc5d47c1aabc18c
widiantoko <dodiwidiantoko@gmail.com>
2023-08-23 20:13:41 +0700

    add file

--- peanut.py
+++ peanut.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import openpyxl
 
 #df=pd.read_csv('intracity_jan_jul_2022.csv')
 #df=pd.read_csv('https://github.com/widiantoko/upload/blob/main/intracity_jan_jul_2022.csv')

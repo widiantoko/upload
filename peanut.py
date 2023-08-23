f61b3b7bfbd3c76ed6454b245754c9f89b4aa27e
widiantoko <dodiwidiantoko@gmail.com>
2023-08-23 23:04:28 +0700

    plotly

--- peanut.py
+++ peanut.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import openpyxl
 import plotly.graph_objects as go
+import plotly_express as px
 import numpy as np
 
 st.set_page_config(page_title="Storage", layout='wide')

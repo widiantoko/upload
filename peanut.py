9c3f6f3d0849794bcff855a4c04aacb2f95d5652
widiantoko <dodiwidiantoko@gmail.com>
2023-08-23 18:54:56 +0700

    update

--- peanut.py
+++ peanut.py
@@ -2,5 +2,7 @@ import pandas as pd
 import streamlit as st
 
 df=pd.read_csv('intracity_jan_jul_2022.csv')
+df=pd.read_csv('https://github.com/widiantoko/upload/blob/main/intracity_jan_jul_2022.csv')
+
 st.dataframe(df.head(10))
 

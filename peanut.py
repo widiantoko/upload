430ee39c658bb580b2376b6093f1b2aa134f0f40
widiantoko <dodiwidiantoko@gmail.com>
2023-08-23 23:49:29 +0700

    round

--- peanut.py
+++ peanut.py
@@ -63,9 +63,9 @@ lokasi['y_loc'] = lokasi['set_loc'].str[6:8]
 
 romox_join=pd.merge(lokasi,romox, left_on="set_loc", right_on='loc', how='outer')
 romox_join= romox_join.fillna(value=np.nan)
-romox_join["bag"] = romox_join["bag"].fillna(0).array.astype(int)
+romox_join["bag"] = round(romox_join["bag"].fillna(0))
 
-romox_join["batch"] = romox_join["batch"].fillna(0).array.astype(int)
+romox_join["batch"] = round(romox_join["batch"].fillna(0))
 
 
 for i, row in romox_join.iterrows():

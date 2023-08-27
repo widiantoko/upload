5f7077a865bea6f5e7d5598d96d1eeb4a734df98
widiantoko <dodiwidiantoko@gmail.com>
2023-08-27 12:26:32 +0700

    update minggu

--- lokalonly.py
+++ lokalonly.py
@@ -13,9 +13,19 @@ from bokeh.models import ColumnDataSource, Range1d, LabelSet
 st.set_page_config(page_title="Storage", layout='wide')
 
 
+romox = pd.read_excel("data/23_08_23_update.xlsx")
+romox["c3"]=romox["c3"].astype(str)
 
 
-romox = pd.read_excel("data/22agustus.xlsx")
+for i, row in romox.iterrows():
+        hasil2 = ''
+        if len(row['c3']) == 1:
+            hasil2 = "0"+row["c3"]
+        else:
+             hasil2 = row["c3"]
+        
+        romox.at[i, 'c3'] = hasil2
+
 
 
 lst = []
@@ -26,41 +36,19 @@ for each in romox["lotno"]:
 final_list = [i for i in lst]
 romox["lotno"]=final_list
 
-warna_bag = {'WMS_n.a.':'#64B5F6',
-        'good stock':'#EF9A9A',
-        'damage':'#FF8A80',
-        'empty':'#1DE9B6'}
 
-#warna_bag = {'WMS_n.a.':'#69F0AE',  'good stock':'#EF9A9A','damage':'#CE93D8', 'empty':'#FB8C00'}
-
-
-#warna_bag = {'good stock':'#FB8C00','WMS_n.a.':'#FFECB3','damage':'#FF8F00','empty':'#69F0AE'}
 
 
 ticktext=['WMS_n.a.', 'good stock', 'damage', 'empty']
 
-dpn=["AA","AB", "AC", "AD", "AE", "AF",
-     "AG", "AH", "BA","BB","BC","BD","BE","BF",
-    "CA","CB","CC","CD","CE","CF"]
 
+dpn=sorted(set(romox["c2"]))
+tgh=sorted(set(romox["c3"]))
+y_loc=sorted(set(romox["c4"]))
 
-
-tgh=["01","02","03", "04","05","06","07","08","09","10","11",
-     "12","13","14","15","16","17","18","19","20","21","22","23",
-     "24","25","26","27","28","29","30","31","32","33","34","35",
-     "36","37","38","39","40","41","42","43","44","45","46","47",
-     "48","49","50","51","52","53",'54',"55","56","57","58","59",
-     "60","61","62","63"
-     ]
 
 dpn_tgh=[(a, b) for a in dpn for b in tgh]
 x_loc_new=[x+"."+y for (x, y) in dpn_tgh]
-
-
-
-y_loc=[
-"1A","1B","1C","1D","1E","1F","1G",
-"1H","1I","1J","1K","1L","1M","1N"]
 
 
 dpn_tgh_blk=[(a, b) for a in x_loc_new for b in y_loc]
@@ -94,19 +82,25 @@ for i, row in romox_join.iterrows():
         
         romox_join.at[i, 'grup'] = hasil
         
-romox_join["warna"]=[warna_bag[x] for x in romox_join["grup"]]
+
+
+color_continuous_scale=[(0.00, "#64B5F6"), (0.25, "#64B5F6"),
+                        (0.25, "#EF9A9A"), (0.50, "#EF9A9A"),
+                        (0.50, "#FF8A80"), (0.75, "#FF8A80"),
+                        (0.75, "#1DE9B6"),  (1.00, "#1DE9B6")]
+
 
 
 for i, row in romox_join.iterrows():
         hasil1 = ''
         if row['loc'] != row['set_loc']:
-            hasil1 = 0
+            hasil1 = 0.4
         elif (row['loc'] == row['set_loc'] and  row['typedesc'] =="GOOD STOCK" and row['qtybag']!=0):
-            hasil1 = 10
+            hasil1 = 0.2
         elif (row['loc'] == row['set_loc'] and  row['typedesc'] =="BLOCKED STOCK" and row['qtybag']!=0):
-            hasil1 = 5
+            hasil1 = 0.8
         else:
-             hasil1 = 3
+             hasil1 = 0.6
         
         romox_join.at[i, 'Z_value'] = hasil1
 
@@ -115,7 +109,7 @@ romox_join["con"] = romox_join['grup'].astype(str)+" : " +romox_join['lotno'].as
 
 
 new_title = '<p style="font-family:sans-serif; font-size: 20px;">Storage Location Control </p>'
-#st.markdown(new_title, unsafe_allow_html=True)
+
  
 pilih_zona=romox_join['zona'].drop_duplicates().sort_index(ascending=True)
 
@@ -126,17 +120,36 @@ col1, col2 = st.columns([1, 12], gap="small")
 with col1:
     
     pilihan=st.selectbox(label="**Location:**",options= pilih_zona)
+    romox_join_zona=romox_join[romox_join.zona == pilihan].reset_index(drop=True)
+
+    x_sum = int(romox_join_zona['qtybag'].sum())
+
+    
+   
+    test= f"""<p style=' color: #3288bd; text-align: center;
+                        font-size: 18px; 
+                        border-radius: 8px; 
+                        border: 2px solid #3288bd;
+                        padding-left: 0px; 
+                        padding-top: 25px; 
+                        padding-bottom: 25px;
+                        line-height:3px;'>
+                        {x_sum} Bag
+                        """
+
+    st.markdown(test, unsafe_allow_html=True)
+
 
 
 
 with col2:
    
-    romox_join_zona=romox_join[romox_join.zona == pilihan].reset_index(drop=True)
+    
         
     hm_zona = go.Figure(go.Heatmap(x=romox_join_zona["x_loc"], y = romox_join_zona["y_loc"], z=romox_join_zona["Z_value"],
                            customdata=romox_join_zona["con"], xgap=1.5, ygap=1.5,text=romox_join_zona["qtybag"],texttemplate="%{text}",
                            textfont={"size":10},
-                           colorscale=romox_join["warna"], showscale=False,
+                           colorscale=color_continuous_scale, showscale=False,
                            hovertemplate="%{x}.%{y} : %{customdata} <extra></extra>"))
 
 
@@ -180,7 +193,8 @@ color_don=['#ef9a9a','#f48fb1','#ce93d8','#b39ddb','#9fa8da','#90caf9',
 
 
 
-diff_sort=sorted(set(aging_sort["diff_days"]))
-st.text(diff_sort)
+#st.text(dpn)
+#st.text(c3_sort)
+
 
 

4720de131239a7930950bf80b7d4fa3d294165a0
widiantoko <dodiwidiantoko@gmail.com>
2023-08-27 12:28:43 +0700

    minggu

--- peanut.py
+++ peanut.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import streamlit as st
-import openpyxl
+
 import plotly.graph_objects as go
 import numpy as np
 
 st.set_page_config(page_title="Storage", layout='wide')
 
-file_01="data/xwh_romo.xlsx"
 
-#wb=openpyxl.load_workbook(file_01)
-romox = pd.read_excel("data/22agustus.xlsx")
+
+
+romox = pd.read_excel("data/23_08_23_update.xlsx")
 
 
 lst = []
@@ -33,30 +33,12 @@ warna_bag = {'WMS_n.a.':'#64B5F6',
 
 ticktext=['WMS_n.a.', 'good stock', 'damage', 'empty']
 
+dpn=sorted(set(romox["c2"]))
+tgh=sorted(set(romox["c3"]))
+y_loc=sorted(set(romox["c4"]))
 
 
 
-y_loc=[
-"1A","1B","1C","1D","1E","1F","1G",
-"1H","1I","1J","1K","1L","1M","1N"]
-
-
-
-
-
-dpn=["AA","AB", "AC", "AD", "AE", "AF",
-     "AG", "AH", "BA","BB","BC","BD","BE","BF",
-    "CA","CB","CC","CD","CE","CF"]
-
-
-
-tgh=["01","02","03", "04","05","06","07","08","09","10","11",
-     "12","13","14","15","16","17","18","19","20","21","22","23",
-     "24","25","26","27","28","29","30","31","32","33","34","35",
-     "36","37","38","39","40","41","42","43","44","45","46","47",
-     "48","49","50","51","52","53",'54',"55","56","57","58","59",
-     "60","61","62","63"
-     ]
 
 dpn_tgh=[(a, b) for a in dpn for b in tgh]
 x_loc_new=[x+"."+y for (x, y) in dpn_tgh]

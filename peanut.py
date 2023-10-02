cdd530dd8bad860ac3c7db4d6c54c337293a5669
widiantoko <dodiwidiantoko@gmail.com>
2023-10-02 13:38:59 +0700

    c3

--- peanut.py
+++ peanut.py
@@ -10,7 +10,9 @@ st.set_page_config(page_title="Romokalisari", layout='wide')
 
 
 romox = pd.read_excel("data/29_09_23_goodstock.xlsx")
-romox["c3"]=str(romox["c3"])
+#romox["c3"]=str(romox["c3"])
+
+romox['c3'] = romox["c3"].map(str)
 
 
 for i, row in romox.iterrows():

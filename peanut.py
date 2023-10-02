795649948595e8ed2858344d3449977e4b804cbc
widiantoko <dodiwidiantoko@gmail.com>
2023-10-02 14:04:28 +0700

    titik

--- peanut.py
+++ peanut.py
@@ -45,11 +45,11 @@ y_loc=sorted(set(romox["c4"]))
 
 
 dpn_tgh=[(a, b) for a in dpn for b in tgh]
-x_loc_new=[x+y for (x, y) in dpn_tgh]
+x_loc_new=[x+"."+y for (x, y) in dpn_tgh]
 
 
 dpn_tgh_blk=[(a, b) for a in x_loc_new for b in y_loc]
-loc_all_new=[x+y for (x, y) in dpn_tgh_blk]
+loc_all_new=[x+"."+y for (x, y) in dpn_tgh_blk]
 
 
 lokasi=pd.DataFrame(loc_all_new, columns=["set_loc"])
